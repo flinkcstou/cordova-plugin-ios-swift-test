ddbfb3c2531be364c6f91232777bdc202e724a35
nazar <flinkcstou@gmail.com>
2020-07-10 12:54:14 +0600

    third commit

---
+++ www/helloSwift.js
@@ -0,0 +1,11 @@
+var exec = require('cordova/exec');
+var PLUGIN_NAME = "helloSwift"; // This is just for code completion uses.
+
+var helloSwift = function () {
+};
+
+YourPluginName.yourFunctionName = function (onSuccess, onError) {
+  exec(onSuccess, onError, PLUGIN_NAME, "openCameraTest", []);
+};
+
+module.exports = helloSwift;

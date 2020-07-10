9d2d34637e6794ec53b7e07f76d263f951c3cd95
nazar <flinkcstou@gmail.com>
2020-07-10 20:21:54 +0600

    add test js function

--- www/helloSwift.js
+++ www/helloSwift.js
@@ -4,8 +4,15 @@ var PLUGIN_NAME = "helloSwift"; // This is just for code completion uses.
 var helloSwift = function () {
 };
 
-helloSwift.yourFunctionName = function (onSuccess, onError) {
+helloSwift.openCameraTest = function (onSuccess, onError) {
   exec(onSuccess, onError, PLUGIN_NAME, "openCameraTest", []);
+};
+helloSwift.test = function () {
+  helloSwift.openCameraTest(function (sc) {
+    console.error(sc)
+  }, function (sc) {
+    console.error(sc)
+  })
 };
 
 module.exports = helloSwift;

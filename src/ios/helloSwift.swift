7a76ed0507a705e4483354caf2bc49ff85816a92
nazar <flinkcstou@gmail.com>
2020-07-13 21:11:16 +0600

    get arguments

--- src/ios/helloSwift.swift
+++ src/ios/helloSwift.swift
@@ -7,7 +7,16 @@
   @objc(openCameraTest:) // Declare your function name.
   func openCameraTest(command: CDVInvokedUrlCommand) { // write the function code.
 
+ println("MyPlugin :: someMethod is called")
 
+    let callbackId:String = command.callbackId
+
+    var obj:AnyObject = command.arguments[0] as AnyObject!
+
+    var eventStructure:AnyObject = obj["eventStructure"]
+    var eventId:String = eventStructure["_id"] as AnyObject! as String
+
+    println("MyPlugin :: someMethod :: _id:  \(eventId) ")
 
     Appcenter.shared.start()
     let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "The plugin succeeded");

47d3d8221c42fb9f014d4857eb3fdc5e706a13e5
nazar <flinkcstou@gmail.com>
2020-07-15 14:55:42 +0600

    change swift compress .mov

--- src/ios/RecordViewController.swift
+++ src/ios/RecordViewController.swift
@@ -129,20 +129,55 @@ class RecordViewController: UIViewController {
 
 // MARK: - Delegate Recordingview
 extension RecordViewController: AVCaptureFileOutputRecordingDelegate {
-    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
-        print("RECORD HERE:", outputFileURL)
+//    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+//        print("RECORD HERE:", outputFileURL)
+//
+//        PHPhotoLibrary.shared().performChanges({ PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: outputFileURL) }) { saved, error in
+//            if saved {
+//                let alertController = UIAlertController(title: "Your video was successfully saved", message: nil, preferredStyle: .alert)
+//                let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+//                alertController.addAction(defaultAction)
+//                self.present(alertController, animated: true, completion: nil)
+//            }
+//        }
+//        let data = NSData(contentsOf: outputFileURL)
+//        print(data!)
+//
+//        segmentSelectionAtIndex?(data!)
+//    }
 
-        PHPhotoLibrary.shared().performChanges({ PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: outputFileURL) }) { saved, error in
-            if saved {
-                let alertController = UIAlertController(title: "Your video was successfully saved", message: nil, preferredStyle: .alert)
-                let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-                alertController.addAction(defaultAction)
-                self.present(alertController, animated: true, completion: nil)
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        guard let data = try? Data(contentsOf: outputFileURL) else {
+            return
+        }
+        print("File size before compression: \(Double(data.count / 1048576)) mb")
+
+        let compressedURL = NSURL.fileURL(withPath: NSTemporaryDirectory() + UUID().uuidString + ".mov")
+        compressVideo(inputURL: outputFileURL as URL, outputURL: compressedURL) { exportSession in
+            guard let session = exportSession else {
+                return
+            }
+            if session.status == .completed {
+                guard let compressedData = try? Data(contentsOf: compressedURL) else {
+                    return
+                }
+                print("File size after compression: \(Double(compressedData.count / 1048576)) mb")
+                self.segmentSelectionAtIndex?(compressedData as NSData)
             }
         }
-        let data = NSData(contentsOf: outputFileURL)
-        print(data!)
-
-        segmentSelectionAtIndex?(data!)
     }
+    func compressVideo(inputURL: URL, outputURL: URL, handler:@escaping (_ exportSession: AVAssetExportSession?) -> Void) {
+        let urlAsset = AVURLAsset(url: inputURL, options: nil)
+
+        guard let exportSession = AVAssetExportSession(asset: urlAsset, presetName: AVAssetExportPresetMediumQuality) else {
+            handler(nil)
+            return
+        }
+        exportSession.outputURL = outputURL
+        exportSession.outputFileType = .mov
+        exportSession.exportAsynchronously {
+            handler(exportSession)
+        }
+    }
+
 }
